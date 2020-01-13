170e4e7fbbdffd7772e10468b98de525622cc784
Yaroslav Kosovych <bassicinbox@gmail.com>
2020-01-13 23:28:35 +0200

    added circle and rect + live parsel

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 let prism = require('prismjs');
 let loadLanguage = require('prismjs/components');
+const textArea = document.getElementById('ta-code');
 
-const code = document.getElementById('code-1').innerHTML;
+let code = document.getElementById('code').innerHTML;
 
 const html = Prism.highlight(code, Prism.languages.html, 'html');
+textArea.value = code;
 document.getElementById('output').innerHTML = html;
+
+textArea.addEventListener('input', (event) => {
+  let value = event.target.value;
+  const html = Prism.highlight(value, Prism.languages.html, 'html');
+  document.getElementById('output').innerHTML = html;
+
+  let code = document.getElementById('code');
+  const parser = new DOMParser();
+  const el = parser.parseFromString(value, 'image/svg+xml');
+  code.innerHTML = '';
+  code.appendChild(el.documentElement);
+});
